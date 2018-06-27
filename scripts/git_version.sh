c9161484844bfaafe082a73c6cb4a363a3fbbe04
Ted Wexler <ted@stuckinacan.com>
2018-06-26 19:39:25 -0700

    Add github releases on tags

--- scripts/git_version.sh
+++ scripts/git_version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 [[ "${TRAVIS}" == "true" ]] && {
-    [[ "${TRAVIS_TAG}" != "" ]] && echo ${TRAVIS_TAG}
+    [[ "${TRAVIS_TAG}" != "" ]] && { echo ${TRAVIS_TAG}; exit; }
     echo "${TRAVIS_COMMIT}" | cut -c -7
 } || {
     echo $(git describe)
